cb56685cc767afa0ab1ddf7ce6ba1813de0dfd0e
Byron Edmonds Decosse <60893158+ByronED@users.noreply.github.com>
2020-03-10 12:49:10 -0400

    numberguessing

---
+++ NumberGuessingByron/NumberGuessingByron/frmNumberGuessing.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace NumberGuessingByron
+{
+    partial class frmNumberGuessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInteger = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(297, 190);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(294, 131);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(131, 13);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Pick a number from (1-10) ";
+            this.lblQuestion.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(517, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // txtInteger
+            // 
+            this.txtInteger.Location = new System.Drawing.Point(520, 123);
+            this.txtInteger.Name = "txtInteger";
+            this.txtInteger.Size = new System.Drawing.Size(100, 20);
+            this.txtInteger.TabIndex = 3;
+            // 
+            // frmNumberGuessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInteger);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnGuess);
+            this.Name = "frmNumberGuessing";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInteger;
+    }
+}
+
